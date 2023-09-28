0df09872dd0867404398f4df1f1d805067763c48
s183576 <s183576@student.dtu.dk>
2023-09-28 10:23:33 +0200

    ny 90 reservoir

--- 7. Helper functions/90_scenario_reservoir.jl
+++ 7. Helper functions/90_scenario_reservoir.jl
@@ -14,21 +14,20 @@ function scenario_90_reservoir(po_cap_s, kWh_cap_s)
     Scenario_dataset_2D = dropdims(aggregated_Scenario_dataset, dims=2)
     # we now have a [1:1440,1:userdefined] dimensioned matrix
 
-    ### now the 90p "worst" scenario is found and thereafter stored in second_worst_scenarios
-    worst_scenarios_90p = []
+   ### now the 90p "worst" scenario is found and thereafter stored in second_worst_scenarios
+   worst_scenarios_90p = []
 
-    for md in 1:M_d
-        # Sort the scenarios in the md-th set
-        sorted_indices = sortperm(Scenario_dataset_2D[md, :])
-        
-        # The index of the 90p "worst" scenario in the md-th set is the (S*(1-0.9)) element in sorted_indices where the worst scenario = the lowest valued element
-        second_worst_index = sorted_indices[(S*(1-0.9))]  
-        
-        # Store the index and outcome of the second-worst scenario
-        push!(second_worst_scenarios, Scenario_dataset_2D[md, second_worst_index])
-    end
+   for md in 1:M_d
+       # Sort the scenarios in the md-th set
+       sorted_indices = sort(Scenario_dataset_2D[md, :])
 
-    return second_worst_scenarios
+       # The index of the 90p "worst" scenario in the md-th set is the (S*(1-0.9)) element in sorted_indices where the worst scenario = the lowest valued element
+       second_worst_index = sorted_indices[Int64(round(S*(1-0.9)+1))]
+
+       # Store the index and outcome of the second-worst scenario
+       push!(worst_scenarios_90p, second_worst_index)
+   end
+
+   return worst_scenarios_90p
 
 end
-
